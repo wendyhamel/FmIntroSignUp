e8bf9f7acb1594267854d706fc130294f462487f
wendyhamel <whamel@it4web.nl>
2023-10-03 17:30:41 +0200

    Styling responsive and error messages

---
+++ src/tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Poppins', 'sans-serif']
+			},
+			colors: {
+				'white': 'hsl(0, 0%, 100%)',
+				'red': 'hsl(0, 100%, 74%)',
+				'green': 'hsl(154, 59%, 51%)',
+				'green-dark': 'hsl(154,64%,40%)',
+				'blue': 'hsl(248, 32%, 49%)',
+				'blue-dark': 'hsl(249, 10%, 26%)',
+				'blue-gray': 'hsl(246, 25%, 77%)',
+			},
+			boxShadow: {
+				'large-b': '0 8px 1px 0 rgba(0, 0, 0, 0.15)',
+			}
+		}
+	},
+}
---
+++ src/validation.js
@@ -0,0 +1,80 @@
+window.signup = function () {
+	return {
+		firstname: '',
+		lastname: '',
+		email: '',
+		password: '',
+		validation: {
+			firstname: {
+				rule: {
+					required: function(field){
+						if (field) {
+							return { invalid: false, message: '' }
+						} else {
+							return { invalid: true, message: 'First Name cannot be empty' }
+						}
+					},
+				}
+			},
+			lastname: {
+				rule: {
+					required: function(field){
+						if (field) {
+							return { invalid: false, message: '' }
+						} else {
+							return { invalid: true, message: 'Last Name cannot be empty' }
+						}
+					},
+				}
+			},
+			email: {
+				rule: {
+					required: function(field){
+						if (field) {
+							return { invalid: false, message: '' }
+						} else {
+							return { invalid: true, message: 'Email cannot be empty' }
+						}
+					},
+					email: function (field) {
+						const validEmailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/g
+						if (validEmailRegex.test(field)) {
+							return { invalid: false, message: '' }
+						} else {
+							return { invalid: true, message: 'Looks like this is not an email' }
+						}
+					}
+				}
+			},
+			password: {
+				rule: {
+					required: function(field){
+						if (field) {
+							return { invalid: false, message: '' }
+						} else {
+							return { invalid: true, message: 'Password cannot be empty' }
+						}
+					},
+				}
+			},
+		},
+		validate(field) {
+			for (const key in this.validation[field].rule) {
+				const validationResult = this.validation[field].rule[key](this[field])
+				if (validationResult.invalid) {
+					this.validation[field].invalid = true
+					this.validation[field].message = validationResult.message
+					break
+				}
+				this.validation[field].invalid = false
+				this.validation[field].message = ''
+			}
+		},
+		submit() {
+			this.validate('firstname')
+			this.validate('lastname')
+			this.validate('email')
+			this.validate('password')
+		}
+	}
+}
